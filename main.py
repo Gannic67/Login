ea4c339608c1ad64c17bfdc31b45d1d412ab2f09
maks67 <gannicus182@mail.ru>
2024-06-08 21:32:29 +0300

    исправлены ошибки

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
-def main():
+def get_menu() -> None:
     menu = int(input('Выберите пункт меню: 1. Вход, 2. Регистрация, 3. Выход'))
     if menu == 1:
-       log()
+        get_login()
     if menu == 2:
-       reg()
+        get_registration()
     if menu == '3':
         exit()
 
 
-def reg():
-    login = input('логин: ')
-    password = input(' пароль')
-    with open('67', 'r') as file:
-        for i in file.readlines():
-            user = i.split(':')
-            if login == user[0]:
-                print('имя занято')
-        if len(login) >= 3 and len(login) <= 20:
-                print('Логин от 3 до 20 символов')
-                reg()
-        if len(password) >= 4 and len(password) <= 32:
-                print('пароль от 3 до 20 символов')
-                reg()
-    with open('67', 'a') as file:
+def get_registration() -> None:
+    try:
+        login = int(input('логин: '))
+        password = int(input(' пароль'))
+        with open('Baza', 'r') as file:
+            for i in file.read().split('\n'):
+                user = i.split(':')
+        if login == user[0]:
+            print('имя занято')
+        if login in range(3, 15):
+            print('Логин должен быть от 3 до 15 символов')
+            get_registration()
+        if password in range(3, 20):
+            print('пароль должен быть от 3 до 20 символов')
+            get_registration()
+        with open('Baza', 'a') as file:
             file.write(f'{login}:{password}\n')
+    except ValueError:
+        print('Вы ввели буквы, а надо цифры')
 
-def log():
-    login = input('логин: ')
-    with open('67', 'r') as file:
-        for i in file.readlines():
-            user = i.split(':')
-            if login == user[0]:
-                password = input(' пароль')
-                if password == user[1]:
-                    print(f' Привет {login}')
-                    exit()
+
+def get_login() -> None:
+    try:
+        login = input('логин: ')
+        with open('Baza', 'r') as file:
+            for i in file.read().split('\n'):
+                user = i.split(':')
+                if login == user[0]:
+                    password = input('пароль: ')
+                    if password == user[1]:
+                        print(f' Привет {user[0]}')
+                        exit()
+
+    except ValueError:
+        print('Вы ввели буквы, а надо цифры')
+
+def check_file():
+    try:
+        with open('Baza', 'r') as file:
+            print('Файл найден')
+    except FileNotFoundError:
+        print('Файл не найден')
+
 
 if __name__ == '__main__':
-    main()
+    get_menu()
